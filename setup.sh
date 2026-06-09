6371c2b4fa9197912ed3dfc5ad80a49403158ae7
Krishnan Shankar <krishnans2006@gmail.com>
2026-06-09 16:49:44 -0400

    Add notes for setup steps

--- setup.sh
+++ setup.sh
@@ -9,3 +9,20 @@ if [ -t 1 ]; then
     exec zsh -l
 fi
 EOF
+
+# Notes:
+# - zsh-bin submodule
+# - zsh-5.8-linux-x86_64.tar.gz from https://github.com/romkatv/zsh-bin/releases/download/v6.1.1/zsh-5.8-linux-x86_64.tar.gz
+# - Run as: ./install -f zsh-5.8-linux-x86_64.tar.gz
+# - oh-my-zsh submodule
+# - cp oh-my-zsh ~/.oh-my-zsh
+# - Run scripts/install-oh-my-zsh.sh
+# - Plugins as submodules: powerlevel10k, zsh-autosuggestions, zsh-syntax-highlighting
+# - Dotfiles as submodule, copied to ~/.dotfiles
+# - Follow steps in ~/.dotfiles/setup_ews.sh to symlink .zshrc, .p10k.zsh, aliases.zsh
+# - gitstatusd from https://github.com/romkatv/gitstatus/releases/download/v1.5.4/gitstatusd-linux-x86_64.tar.gz
+# - Extract with tar
+# - Copy the executable (gitstatusd-linux-x86_64) to ~/.cache/gitstatus
+# - Other general dotfiles setup (git, etc.)
+# - Some confirmation that everything works
+# - Add $BASHRC_HEADING above to .bashrc to launch zsh
